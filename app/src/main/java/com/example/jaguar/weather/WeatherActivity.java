05795c8b8720fabfb7e1f318b50d91ffe1b4bd29
jjjaga <jjjaga@yandex.ru>
2018-10-31 23:40:38 +0300

    add image weather state

--- app/src/main/java/com/example/jaguar/weather/WeatherActivity.java
+++ app/src/main/java/com/example/jaguar/weather/WeatherActivity.java
@@ -1,16 +1,24 @@
 package com.example.jaguar.weather;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.jaguar.weather.Resources.ConstApi;
 
 import org.json.JSONObject;
 
+import java.io.InputStream;
+
 public class WeatherActivity extends AppCompatActivity {
     TextView text;
+    ImageView imageWeather;
+    public String UrliconWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,12 @@ public class WeatherActivity extends AppCompatActivity {
 
         text = findViewById(R.id.hello);
         text.setText("sdf");
+        imageWeather = findViewById(R.id.imageWeather);
 
         UpdateWeather upd = new UpdateWeather();
-        upd.city = "Москва"; upd.lang = "ru"; upd.days = "7";
+        upd.city = "Moscow"; upd.lang = "ru"; upd.days = "7";
         upd.execute();
+
     }
     class UpdateWeather extends AsyncTask<Void, Void, JSONObject> {
         String city; String lang; String days;
@@ -40,15 +50,41 @@ public class WeatherActivity extends AppCompatActivity {
             parse.parseWeather(json);
         }
     }
+    class ImageLoad extends AsyncTask<String, Void, Bitmap> {
+
+        protected Bitmap doInBackground(String... strings) {
+            String urldisplay = "https:"+UrliconWeather;
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageWeather.setImageBitmap(result);
+        }
+    }
     public class ParseWeather {
         public void  parseWeather(JSONObject json){
             try {
-                String location = json.getJSONObject("location").getString("name").toString();
-                String temp = json.getJSONObject("current").getString("temp_c").toString();
-                String cond = json.getJSONObject("current").getJSONObject("condition").getString("text").toString();
-                text.setText("В городе "+location+" температура " + temp + ""+cond);
+                String location = json.getJSONObject("location").getString("name");
+                String temp = json.getJSONObject("current").getString("temp_c");
+                String speedwind = json.getJSONObject("current").getString("wind_kph");
+                String cond = json.getJSONObject("current").getJSONObject("condition").getString("text");
+                UrliconWeather = json.getJSONObject("current").getJSONObject("condition").getString("icon");
+                text.setText("Город: "+location+"\nt " + temp +"\n"+cond+"\n"+"Скорость ветра "+ speedwind);
+
             }catch(Exception e){
+            }
+            if(UrliconWeather != null) {
+                ImageLoad load = new ImageLoad();
+                load.execute(UrliconWeather);
             }
         }
     }
+
 }
